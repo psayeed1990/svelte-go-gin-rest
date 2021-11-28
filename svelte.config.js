9026ee0a7ce0bd11914e21c8f256a6bf19fb8e14
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-11-28 21:10:27 +0600

    added static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ out: 'build' }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
